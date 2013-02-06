3d26be1ef53da4415043184e5045d9f239328ebf
Kyle Campbell <mail@slajax.com>
2013-02-06 14:43:28 -0800

    fixed some indenting

--- routes/sockets/chat.js
+++ routes/sockets/chat.js
@@ -9,9 +9,9 @@ var messages = [];
 module.exports = function(io,socket) {
 
   socket.on('/chat/server/user/add', function(user){
-      if(users.indexOf(user) == -1) users.push(user);
-      io.sockets.emit('/chat/client/msg/add', '<div><strong>'+user+'</strong> <i>has joined!</i></div>');
-      io.sockets.emit('/chat/client/user/add', users);
+    if(users.indexOf(user) == -1) users.push(user);
+    io.sockets.emit('/chat/client/msg/add', '<div><strong>'+user+'</strong> <i>has joined!</i></div>');
+    io.sockets.emit('/chat/client/user/add', users);
   });
 
   socket.on('/chat/server/user/rm', function(user) {
@@ -21,8 +21,8 @@ module.exports = function(io,socket) {
   });
 
   socket.on('/chat/client/msg/add', function(msg){
-      if(msg && users.indexOf(msg) == -1) messages.push(msg);
-      if(msg) io.sockets.emit('/chat/client/msg/add', '<div><strong>'+msg.user+'</strong>: '+msg.data.toString()+'</div>');
+    if(msg && users.indexOf(msg) == -1) messages.push(msg);
+    if(msg) io.sockets.emit('/chat/client/msg/add', '<div><strong>'+msg.user+'</strong>: '+msg.data.toString()+'</div>');
   });
 
 };
